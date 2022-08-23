2dd458c45d195c937d81a9acf2dbccb6dfaf1249
Miguel Rocha <miguelrocha.dev@gmail.com>
2022-08-23 01:42:29 +0100

    feat: add english language translation

---
+++ locales/en.ts
@@ -0,0 +1,27 @@
+import { Language } from '.';
+
+const en: Language = {
+  title: 'Welcome to',
+  description: 'Get started by editing',
+  grid: [
+    {
+      title: 'Documentation',
+      paragraph: 'Find in-depth information about Next.js features and API.',
+    },
+    {
+      title: 'Learn',
+      paragraph: 'Learn about Next.js in an interactive course with quizzes!',
+    },
+    {
+      title: 'Examples',
+      paragraph: 'Discover and deploy boilerplate example Next.js projects.',
+    },
+    {
+      title: 'Deploy',
+      paragraph: 'Instantly deploy your Next.js site to a public URL with Vercel.',
+    },
+  ],
+  footer: 'Powered by',
+};
+
+export default en;
