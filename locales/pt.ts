35fe28bc855b5108607c10b4390a168b608eba23
Miguel Rocha <miguelrocha.dev@gmail.com>
2022-08-23 01:43:01 +0100

    feat: add portuguese language translation

---
+++ locales/pt.ts
@@ -0,0 +1,27 @@
+import { Language } from '.';
+
+const pt: Language = {
+  title: 'Bem-vindo ao',
+  description: 'Comece editando',
+  grid: [
+    {
+      title: 'Documentação',
+      paragraph: 'Encontre informações detalhadas sobre as funcionalidades e a API do Next.js.',
+    },
+    {
+      title: 'Aprenda',
+      paragraph: 'Aprenda sobre Next.js em um curso interativo com quizzes!',
+    },
+    {
+      title: 'Exemplos',
+      paragraph: 'Descubra e faça deploy de projetos boilerplate Next.js.',
+    },
+    {
+      title: 'Deploy',
+      paragraph: 'Faça deploy de seu site Next.js em uma URL pública com a Vercel em poucos instantes.',
+    },
+  ],
+  footer: 'Desenvolvido por',
+};
+
+export default pt;
